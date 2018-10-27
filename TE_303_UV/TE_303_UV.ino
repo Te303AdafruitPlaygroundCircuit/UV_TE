a786d4d2031fc43efaec55522eb18b42e4516c3e
Hansen Liang <>
2018-10-27 13:11:19 -0400

    Added color instant meter display functionality

--- TE_303_UV/TE_303_UV.ino
+++ TE_303_UV/TE_303_UV.ino
@@ -10,37 +10,80 @@
 #include <Adafruit_CircuitPlayground.h>
 #include <Adafruit_Circuit_Playground.h>
 
-const int UvSensorPin = A4;
+const int UV_SENSE_PIN = A4;
+const int LED_COUNT = 10;
 
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
-  // Adafruit_CircuitPlayground.strip.setPixelColor(1, 255); comment it out for a section
-  //CircuitPlayground.setPixelColor(2, 255,255,255);
+  CircuitPlayground.begin();
+  
 }
 
 void loop() {
   // read the input on analog pin 0:
-  int sensorValue = analogRead(UvSensorPin);
-  // print out the value you read:
+  int sensorValue = analogRead(UV_SENSE_PIN);
   Serial.println(sensorValue);
+  
   delay(1);        // delay in between reads for stability
-
-  CircuitPlayground.setPixelColor(0, 245,235,255);
+  
+  int instReading = map(sensorValue, 0, 127, 0, LED_COUNT); // scale reading to LEDs
+  showMeter(instReading);
 }
 
-//// show a "meter" with the ring of LEDs on the board. Input how many lights should go on
-//void showMeter(int ledCount) {  
-//  //Failsafe
-//  if (ledCount > 10){
-//    ledCount = 10
-//  }
-//
-//  else if (ledCount < 0) {
-//    ledCount = 0
-//  }
-//
-//  else{
-//    
-//  }
-//}
+// show a "meter" with the ring of LEDs on the board. Input how many lights should go on
+void showMeter(int meterValue) {  
+
+  
+  //Limiter
+  if (meterValue > LED_COUNT){
+    meterValue = LED_COUNT;
+  }
+
+  if (meterValue < 0) {
+    meterValue = 0;
+  }
+
+
+  // light up the LEDs in their pre-assigned colors while remapping to intuitive order
+  switch (meterValue) {
+    case 10: CircuitPlayground.setPixelColor(5, 0xff0000);
+    case 9: CircuitPlayground.setPixelColor(6, 0xff0000);
+    case 8: CircuitPlayground.setPixelColor(7, 0xf94e00);
+    case 7: CircuitPlayground.setPixelColor(8, 0xf9a600);
+    case 6: CircuitPlayground.setPixelColor(9, 0xf9db00);
+    case 5: CircuitPlayground.setPixelColor(0, 0xaaf900);
+    case 4: CircuitPlayground.setPixelColor(1, 0x52f900);
+    case 3: CircuitPlayground.setPixelColor(2, 0x00f918);
+    case 2: CircuitPlayground.setPixelColor(3, 0x00bef9);
+    case 1: CircuitPlayground.setPixelColor(4, 0x005ff9);
+  }
+
+  clearOtherLEDs(meterValue);
+  
+}
+
+// turns off all LEDs (somehow the default .clearPixels() doesn't work...)
+void clearLEDs(){
+  for (int i = 0; i < LED_COUNT; i++){
+    CircuitPlayground.setPixelColor(i, 0x000000);
+  }
+}
+
+// turn the rest of the LEDs not being used
+void clearOtherLEDs(int meterValue){
+  // turn off LEDs while remapping to intuitive order
+  switch (meterValue) {
+    case 1: CircuitPlayground.setPixelColor(4, 0x000000);
+    case 2: CircuitPlayground.setPixelColor(3, 0x000000);
+    case 3: CircuitPlayground.setPixelColor(2, 0x000000);
+    case 4: CircuitPlayground.setPixelColor(1, 0x000000);
+    case 5: CircuitPlayground.setPixelColor(0, 0x000000);
+    case 6: CircuitPlayground.setPixelColor(9, 0x000000);
+    case 7: CircuitPlayground.setPixelColor(8, 0x000000);
+    case 8: CircuitPlayground.setPixelColor(7, 0x000000);
+    case 9: CircuitPlayground.setPixelColor(6, 0x000000);
+    case 10: CircuitPlayground.setPixelColor(5, 0x000000);
+    
+  }
+}
