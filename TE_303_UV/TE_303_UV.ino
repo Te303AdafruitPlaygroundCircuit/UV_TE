02271669fa0d5bdf27f11c0f47bd20263841d18d
Hansen Liang <>
2018-12-03 16:46:35 -0500

    Code is functional. IN DEMO MODE -- the accumulation function works super quickly for demo. Change the timer call frequency to every MINUTE (60000ms) to fix.

--- TE_303_UV/TE_303_UV.ino
+++ TE_303_UV/TE_303_UV.ino
@@ -1,34 +1,58 @@
 /*
   UV Project
   TE 303 Prototyping
+
+  This UV project has 2 modes: 
+  Slide switch - left: Instant UV meter mode will show # of LEDs based on the currently measured UV Index
+  Slide switch - right: Cumulative UV exposure mode (currenlty tuned to a demo mode, which will go off  quickly) keeps adding how much exposure you've had and plays an alarm when you've exceeded your daily safe dose of UV.
   
   (c) 2018 Concepcion Sosa & Duheng "Hansen" Liang
  
  */
 
-
+#include <timer.h>
 #include <Adafruit_CircuitPlayground.h>
 #include <Adafruit_Circuit_Playground.h>
 
 const int UV_SENSE_PIN = A4;
 const int LED_COUNT = 10;
+bool leftButtonPressed = false;
+bool muted = false;
+const int UV_EXP_THRESH = 210; // According to EPA, over 30 minutes of UVI 7 exposure can lead to skin damage
+int cuUVExp = 0;
+int instUVI = 0;
+auto timer = timer_create_default();
 
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
   CircuitPlayground.begin();
-  
 }
 
 void loop() {
+  timer.tick();
   // read the input on analog pin 0:
   int sensorValue = analogRead(UV_SENSE_PIN);
-  Serial.println(sensorValue);
   
-  delay(1);        // delay in between reads for stability
+  //delay(1);        // delay in between reads for stability
+
+  leftButtonPressed = CircuitPlayground.leftButton(); // for muting the alarm
+
+
+  // if slideswitch is on the left, it's the instant UV meter mode
+  if(CircuitPlayground.slideSwitch()){
+      showMeter(instUVI);
+  }
+  // if the slideswitch is on the right, show the cumulative exposure
+  else{
+    int currentExposure = map(cuUVExp, 0, UV_EXP_THRESH, 0, LED_COUNT);
+      showMeter(currentExposure);
+  }
+
   
-  int instReading = map(sensorValue, 0, 127, 0, LED_COUNT); // scale reading to LEDs
-  showMeter(instReading);
+  instUVI = map(sensorValue, 0, 1023, 0, 50); // according to Adafruit, the reading 0-5V corresponds linearly to UVI 0 - 50
+  timer.every(1000, addExp); // Every minute, add current UVI to cumulative exposure 
+  
 }
 
 // show a "meter" with the ring of LEDs on the board. Input how many lights should go on
@@ -56,7 +80,8 @@ void showMeter(int meterValue) {
     case 4: CircuitPlayground.setPixelColor(1, 0x52f900);
     case 3: CircuitPlayground.setPixelColor(2, 0x00f918);
     case 2: CircuitPlayground.setPixelColor(3, 0x00bef9);
-    case 1: CircuitPlayground.setPixelColor(4, 0x005ff9);
+    //case 1: CircuitPlayground.setPixelColor(4, 0x005ff9);
+    default: CircuitPlayground.setPixelColor(4, 0x005ff9); // always light 1 up to show it's working.
   }
 
   clearOtherLEDs(meterValue);
@@ -74,6 +99,7 @@ void clearLEDs(){
 void clearOtherLEDs(int meterValue){
   // turn off LEDs while remapping to intuitive order
   switch (meterValue) {
+    case 0:
     case 1: CircuitPlayground.setPixelColor(4, 0x000000);
     case 2: CircuitPlayground.setPixelColor(3, 0x000000);
     case 3: CircuitPlayground.setPixelColor(2, 0x000000);
@@ -86,4 +112,23 @@ void clearOtherLEDs(int meterValue){
     case 10: CircuitPlayground.setPixelColor(5, 0x000000);
     
   }
+}
+
+// add current exposure to the cumulative value. Play alarm if cumulative value is above the threshold.
+bool addExp(void *){
+  cuUVExp += instUVI;
+  if(cuUVExp > UV_EXP_THRESH && !muted) {
+    playAlarm();
+  }
+  Serial.println(cuUVExp);
+  return true; // keep the timer active by returning true
+}
+
+void playAlarm(){
+    CircuitPlayground.playTone(1000,1000);
+    CircuitPlayground.playTone(1340,1000);
+    if(CircuitPlayground.leftButton()){
+      muted = true;
+    }
+    return;
 }
